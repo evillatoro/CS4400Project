8b9347176a01a993dbe460dec4f1c6b295a494a7
lynnyou7 <1020361Teenie!>
2017-04-07 21:46:18 -0400

    addNewLocationController logic added, button labels given

--- src/controller/AddNewLocationController.java
+++ src/controller/AddNewLocationController.java
@@ -2,9 +2,20 @@ package controller;
 
 import fxapp.MainFXApplication;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.TextField;
 
 public class AddNewLocationController {
 
+    @FXML
+    private TextField poiLocationNameField;
+    @FXML
+    private ChoiceBox cityChoiceBox;
+    @FXML
+    private ChoiceBox stateChoiceBox;
+    @FXML
+    private TextField zipCodeField;
     /** a link back to the main application class */
     private MainFXApplication mainApplication;
 
@@ -24,4 +35,41 @@ public class AddNewLocationController {
     private void handleBackPressed() {
         mainApplication.displayFunctionalityCityScientistScene();
     }
+
+    /**
+     * called when the user clicks submit
+     */
+    @FXML
+    private void handleSubmitPressed() {
+        isInputValid();
+
+    }
+    private boolean isInputValid() {
+        String errorMessage = "";
+
+        // check if fields are empty
+        if ((poiLocationNameField.getText() == null) ||
+                (poiLocationNameField.getText().isEmpty())) {
+            errorMessage += "No valid POILocation!\n";
+        }
+        if ((zipCodeField.getText() == null) ||
+                (zipCodeField.getText().isEmpty())) {
+            errorMessage += "No valid zip code!\n";
+        }
+
+        //no error message means success / good input
+        if (errorMessage.isEmpty()) {
+            return true;
+        } else {
+            // Show the error message if bad data
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.initOwner(mainApplication.getWindow());
+            alert.setTitle("Invalid Fields");
+            alert.setHeaderText("Please correct invalid fields");
+            alert.setContentText(errorMessage);
+            alert.showAndWait();
+            return false;
+        }
+    }
+
 }
